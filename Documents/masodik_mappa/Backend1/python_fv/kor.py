3f9d70ce21671efc43bb1429805dbf27139983df
Ganzler Katalin <ganzler@iif.hu>
2023-05-28 21:25:32 +0200

    Pi

--- Documents/masodik_mappa/Backend1/python_fv/kor.py
+++ Documents/masodik_mappa/Backend1/python_fv/kor.py
@@ -1,14 +1,15 @@
 #Python függvények 4.Feladat - Kör kerület és terület számítás
+from math import pi
 
 #def kerulet(szam)
- #  # kerulet = 2 * db * 3,14
- #   return 2 * db * 3,14
+ #  # kerulet = 2 * db * pi
+ #   return 2 * db * pi
 
 szam = input('Kérem a kör sugarát!')
 r = int(szam)
 
-kerulet = 2*r*3.14
-terulet = r*r*3.14
+kerulet = 2*r*pi
+terulet = r*r*pi
 
 print("Kör kerülete: ",kerulet)
 print("Kör területe: ",terulet)
