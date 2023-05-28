3b77e6ee16dbc9ca4e927ab5f7a52828c8519c09
Ganzler Katalin <ganzler@iif.hu>
2023-05-28 17:42:38 +0200

    User generalas

--- Documents/masodik_mappa/Backend1/python_backend/user_generalas.py
+++ Documents/masodik_mappa/Backend1/python_backend/user_generalas.py
@@ -1,14 +1,21 @@
+import unicodedata
+import io
+
 nevsor = [['Kovács', 'Béla'], ['Kiss', 'Gyula'], ['Szabó', 'Ervin']]
 
 print('\n1.Feladat:User generálás')
+
 for index in range(0, len(nevsor)):
-    vezeteknev = nevsor[index][0]
-    keresztnev = nevsor[index][0]
-    print('name:',nevsor[index],',')
-#   print('email:',nevsor[index],',')
-    print('email:',nevsor[index][0]+'.'+nevsor[index][1]+"@company.hu"',')
-#   print('password:',nevsor[index],',')
-    print('password:',nevsor[index][0]+"123Start",',')
-    print("")
+
+#    data = 'Szabó béláaáá145úúő'
+#    normal = unicodedata.normalize('NFKD', data).enc
+#    vezeteknev = nevsor[index][0]
+#    keresztnev = nevsor[index][0]
+#    print('name:',nevsor[index],',')
+     print(nevsor[index][0]+' ' +nevsor[index][1],'',nevsor[index][0]+'.'+nevsor[index][1]+"@company.hu"+' '+nevsor[index][0]+"123Start")
+#    print('email:',nevsor[index][0]+'.'+nevsor[index][1]+"@company.hu"',')
+#    print('password:',nevsor[index][0]+"123Start",',')
+#    print(nevsor[index][0]+"123Start")
+
     
     
